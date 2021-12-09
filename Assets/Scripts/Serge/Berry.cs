641aa6fe0a7c1eaf3f44d375cd649622f6bd4d63
AXLRSR <62110917+AXLRSR@users.noreply.github.com>
2021-12-09 23:34:03 +0100

    :sparkles: Handle Berry animations

--- Assets/Scripts/Serge/Berry.cs
+++ Assets/Scripts/Serge/Berry.cs
@@ -7,14 +7,27 @@ public class Berry : MonoBehaviour
     public Animator animator;
     public GameObject berry;
 
+    private bool isWaiting = false;
+
+    void Start()
+    {
+        GameController.Instance.OnNoBerry += handleNoBerry;
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !GameController.Instance.MenuOpen()) {
-            StartCoroutine(ShowAndHideBerry());
+            if (isWaiting) {
+                StartCoroutine(HideBerry());
 
-            animator.SetTrigger("EatBerry");
-            GameController.Instance.TakeBerry();
+                animator.SetTrigger("TakeBerry");
+                isWaiting = false;
+            } else {
+                StartCoroutine(ShowAndHideBerry());
+
+                animator.SetTrigger("EatBerry");
+            }
         }
     }
 
@@ -23,5 +36,35 @@ public class Berry : MonoBehaviour
         berry.SetActive(true);
         yield return new WaitForSeconds(2f);
         berry.SetActive(false);
+        GameController.Instance.TakeBerry();
+    }
+
+    public IEnumerator ShowBerry() {
+        yield return new WaitForSeconds(0.8f);
+        berry.SetActive(true);
+    }
+
+    void handleNoBerry()
+    {
+        animator.SetTrigger("GetBerry");
+        StartCoroutine(ShowBerry());
+        StartCoroutine(CancelBerry());
+        isWaiting = true;
+    }
+
+    public IEnumerator HideBerry() {
+        yield return new WaitForSeconds(1.3f);
+        berry.SetActive(false);
+        GameController.Instance.TakeBerry();
+    }
+
+    public IEnumerator CancelBerry() {
+        yield return new WaitForSeconds(5f);
+        if (!GameController.Instance.IsOnBerry()) {
+            isWaiting = false;
+            animator.SetTrigger("CancelBerry");
+            yield return new WaitForSeconds(1f);
+            berry.SetActive(false);
+        }
     }
 }
